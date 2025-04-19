44ff6398d4f2c279700c427f3617537ca05ef37f
DaniilAnuchkin <99193294+DaniilAnuchkin@users.noreply.github.com>
2025-04-19 23:43:23 +0300

    Added pagination for monster list (#8)
    Co-authored-by: Daniil Anuchkin <danuchkin@i-dev.online>

--- server/src/services/monster.service.ts
+++ server/src/services/monster.service.ts
@@ -1,3 +1,4 @@
+import type { RootFilterQuery } from "mongoose";
 import monsterModel from "../models/monsters/monster.model";
 import { IMonster } from "../models/monsters/types";
 import { IFetchAllReq, IPaginatedObj } from "@sharedTypes/index";
@@ -6,12 +7,19 @@ const monsterService = {
   getAllMonsters: async (
     req: IFetchAllReq
   ): Promise<IPaginatedObj<IMonster>> => {
+    const query: RootFilterQuery<IMonster> = {
+      $or: [
+        { name: { $regex: req.search || "", $options: "i" } },
+        { description: { $regex: req.search || "", $options: "i" } },
+      ],
+    };
+
     const monsters = await monsterModel
-      .find()
-      .sort({ createdAt: -1 })
+      .find(query)
+      .sort({ [req.field]: req.order })
       .skip(req.page * 10)
       .limit(10);
-    const totalElements = await monsterModel.countDocuments();
+    const totalElements = await monsterModel.countDocuments(query);
     return {
       content: monsters,
       totalElements,
